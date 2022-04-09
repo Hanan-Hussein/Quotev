67087aee2c15a714ef89b03ea9ba664174e8c2b1
Hanan-Hussein <hanhuss035@gmail.com>
2022-04-09 15:49:51 +0300

    ft:quoteClass-A class that holds form details

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
+
 export class CardComponent implements OnInit {
+  @Input() quote = 'Lock up your libraries if you like; but there is no gate, no lock, no bolt that you can set upon the freedom of my mind.';
+  @Input() author = 'Virginia Woolf';
+  @Input() editor = 'Jaffar Hussein'
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+ngOnInit(): void {
+}
 
 }
---
+++ src/app/quotes.ts
@@ -0,0 +1,10 @@
+export class Quotes {
+    constructor(
+        public quote:string,
+        public author:string,
+        public editor:string
+    )
+    {
+
+    }
+}
