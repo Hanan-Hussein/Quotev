2f7d6948a7cad0aa7c4ff66684c746218c6906a3
Hanan-Hussein <hanhuss035@gmail.com>
2022-04-09 22:09:13 +0300

    bg-adding object to card-the cards are blank

--- src/app/card-frame/card-frame.component.ts
+++ src/app/card-frame/card-frame.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 
 @Component({
   selector: 'app-card-frame',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card-frame.component.css']
 })
 export class CardFrameComponent implements OnInit {
-
+@Input('ItemsQuote') quoteItems:any=[]
   constructor() { }
 
   ngOnInit(): void {
   }
-
+getQuote(quote:any,str:string){
+return quote[str]
+}
 }
