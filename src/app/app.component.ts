5de723d7e0aa433211e3ee657cd8ee3e05d3ca20
Hanan-Hussein <hanhuss035@gmail.com>
2022-04-09 23:35:24 +0300

    ft:newlycards-newly quotes are placed on top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Quotev';
   items:any=[{quote:"Lock up your libraries if you like; but there is no gate, no lock, no bolt that you can set upon the freedom of my mind.",
-author:"Virginia Woolf",editor:" HananHussein"}];
+author:"Virginia Woolf",editor:" Hanan Hussein"}];
 
   AddnewQuote = (obj: any) => {
     console.log(obj);
-    this.items.push(obj);
+    this.items.unshift(obj);
 
   };
 }
